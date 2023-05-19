7b50a1867a05895f9f54fde8c2e25def9d155d33
Volodya Leveryev <volodya.leveryev@gmail.com>
2023-05-19 21:22:16 +0900

    Fix

--- shop/views.py
+++ shop/views.py
@@ -9,7 +9,7 @@ def index_page():
     query = models.db.select(models.Phone)
     brand_pk = request.args.get("brand")
     if brand_pk:
-        query = query.filter_by(pk=brand_pk)
+        query = query.filter_by(brand_pk=int(brand_pk))
     phones = models.db.session.execute(query).scalars()
 
     return render_template(
